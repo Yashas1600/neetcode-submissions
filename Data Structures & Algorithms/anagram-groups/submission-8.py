775f01f298072fec5984b9cf3c8f6dca50c1d0dc
Yashas Shashidhara <yashas22@g.ucla.edu>
2026-09-23 14:57:21 -0700

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #hashMap
+        res = defaultdict(list)
+        for str in strs:
+            count = [0] * 26
+            for letter in str:
+                count[ord(letter) - ord('a')] += 1
+            res[tuple(count)].append(str)
+        return list(res.values())
